9a23779a2d54a041f18eb501d7e3454729b6446e
Brandon Weaver <a7xncob@gmail.com>
2019-11-10 06:54:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,27 +14,6 @@ class Artist
     song.artist = self
   end
   
-  def find_or_create_by_name(name)
-    self.all.each do |artist|
-      if artist.name == name
-        return artist
-      end
-    end
-    @name = name
-    @@all << self
-    return artist
-  end
-  
-  def songs
-    return Song.all.select { |song| song.artist == self }
-  end
-  
-  def print_songs
-    @songs.each do |song|
-      puts song
-    end
-  end
-  
   def self.all
     return @@all
   end
